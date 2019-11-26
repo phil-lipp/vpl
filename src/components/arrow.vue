0385f183755b5b7a02df5af62aa2d3bc5f666061
7dammann <57994233+7dammann@users.noreply.github.com>
2019-11-26 20:04:07 +0100

    26.11. 20 Uhr

--- src/components/arrow.vue
+++ src/components/arrow.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
   // name: 'ComponentName',
-  props: ['ht', 'wd'],
+  // props: ['ht', 'wd'],
   data () {
     return {
     }
--- src/components/lane.vue
+++ src/components/lane.vue
@@ -2,7 +2,6 @@
   <div class="q-pa-md">
     <text-caption>Lane {{numb}}</text-caption>
     <q-scroll-area horizontal style="height: 210px;" class="bg-grey-3">
-      <q-img src="https://placeimg.com/500/300/nature" :ratio="1"/>
     </q-scroll-area>
   </div>
 </template>
