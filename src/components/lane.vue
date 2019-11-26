213d21a7c1ae61fc6a44c78b270fa2493b12127b
7dammann <57994233+7dammann@users.noreply.github.com>
2019-11-26 16:05:55 +0100

    26.11. 16 Uhr

--- src/components/lane.vue
+++ src/components/lane.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="q-pa-md">
-    <q-scroll-area horizontal style="height: 210px; width: 1230px;" class="bg-grey-3">
+    <text-caption>Lane {{numb}}</text-caption>
+    <q-scroll-area horizontal style="height: 210px;" class="bg-grey-3">
       <q-img src="https://placeimg.com/500/300/nature" :ratio="1"/>
     </q-scroll-area>
   </div>
